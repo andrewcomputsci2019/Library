9e0e1534c3f130edd7bfc1015dd52621f4003fed
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2020-11-24 12:23:47 -0800

    copyright thing

--- src/Books_removed/Checked_outlist.java
+++ src/Books_removed/Checked_outlist.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020 Feel free to edit this project in any way but you must credit me in your works a simple comment or side note will work just fine have fun.
+ */
+
 package Books_removed;
 
 public class Checked_outlist {
