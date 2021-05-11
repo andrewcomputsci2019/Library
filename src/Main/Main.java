369344f79d235ae5843562c7b96bb3eeb1392401
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-05-10 20:34:58 -0700

    Unix Support

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -118,8 +118,9 @@ public class Main {
             switch (x)
             {
                 case 0: final String os = System.getProperty("os.name");
-                        if (os.contains("windows")){
-                            Runtime.getRuntime().exec("cls"); // windows
+                        if (os.contains("Windows")){
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
                         }
                         else{
                             Runtime.getRuntime().exec("clear"); //unix
